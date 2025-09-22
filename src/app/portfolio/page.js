adb7324859df272b28ae7e19522c960db21fb881
sougata <sougataghar47@gmail.com>
2025-09-22 18:14:01 +0530

    changed portfolio to server component

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,28 +1,19 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import Orders from "@/components/Orders";
 import PortfolioChart from "@/components/PortfolioChart";
 
-export default function Portfolio() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function fetchPortfolio() {
-      try {
-        const response = await fetch(
-          process.env.NEXT_PUBLIC_HOST + "/api/portfolio"
-        );
-        const res = await response.json();
-        setData(res.data);
-        console.log(res.data);
-      } catch (error) {
-        console.error("Failed to fetch portfolio:", error);
-      }
-    }
-
-    fetchPortfolio();
-  }, []);
+export default async function Portfolio() {
+  let data = [];
+  try {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_HOST + "/api/portfolio",
+      { cache: "no-store" } // optional, to always fetch fresh data
+    );
+    const json = await res.json();
+    data = json.data;
+    console.log(data);
+  } catch (error) {
+    console.error("Failed to fetch portfolio:", error);
+  }
 
   return (
     <>
