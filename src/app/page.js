cbf4b4474aca48e6baed918f5abfc58dc472d764
sougata <sougataghar47@gmail.com>
2025-09-22 18:26:56 +0530

    changed page of home

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-4">An EazyBytz Stock Market Website</h1>
-      <img alt="Stock Market" className="w-full" src="https://images.unsplash.com/photo-1648275913341-7973ae7bc9b3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D"/>  
+      <img alt="Stock Market" className="w-full rounded-lg" src="https://images.unsplash.com/photo-1648275913341-7973ae7bc9b3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D"/>  
     </div>
   );
 }
